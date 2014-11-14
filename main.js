317bfde30267c43b6ffd869c6eb20e517fbf3897
anastasiadecleyre <anastasiadecleyre@gmail.com>
2014-11-13 21:57:03 -0600

    main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+function City(name) {
+  this.name = name;
+  this.lots = [];
+}
+
+function Lot(type) {
+  this.population = 0;
+	this.type = type;
+	this.demand = [];
+	this.supply = [];
+}
+
+function Demand(good, quantity) {
+  this.good = good;
+	this.quantity = quantity;
+}
+
+function Supply(good, quantity) {
+  this.good = good;
+	this.quantity = quantity;
+}
+
+function Person() {
+}
+
+function TradeGood() {
+  this.price = 0;
+}
+
+city = new City(Destin);
+city.lots[0] = new Lot("home");
+city.lots[0].demand[0] = new Demand("food", 3);
